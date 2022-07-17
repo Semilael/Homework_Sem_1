4167447cc14f22829af6d6af93d212077a4c5c4d
Semilael <ariana-ses97@yandex.ru>
2022-07-17 16:18:05 +0300

    Program to count max and min from 2 integers

---
+++ MaxFrom2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Эта программа найдет максимальное значение двух чисел. Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine(num1 + " - Это максимальное из двух чисел");
+    Console.WriteLine(num2 + " - Это минимальное из двух чисел");
+}
+else
+{
+    Console.WriteLine(num2 + " - Это максимальное из двух чисел");
+    Console.WriteLine(num1 + " - Это минимальное из двух чисел");
+}
