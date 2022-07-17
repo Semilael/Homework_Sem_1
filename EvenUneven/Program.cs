db718372c4e288e1431b8cd3291fd9f46fc67d82
Semilael <ariana-ses97@yandex.ru>
2022-07-17 17:49:28 +0300

    Project to name if even or uneven

---
+++ EvenUneven/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Эта программа выведет четное число или нет. Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+if(num1 % 2 == 0)
+{
+    Console.WriteLine(num1 + " - Это четное число.");
+}
+else
+{
+    Console.WriteLine(num1 +" - Это нечетное число.");
+}
