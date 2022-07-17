d5a7dbdcb20f8ddb72ddce0ee7dc4c2183501540
Semilael <ariana-ses97@yandex.ru>
2022-07-17 17:33:18 +0300

    Project counts from 1 to N all the even numbers

---
+++ 1toN/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Эта программа выведет ряд четных чисел до введенного числа. Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int CountN = 1;
+
+while(CountN < N)
+{
+    if(CountN % 2 == 0)
+    {
+        Console.WriteLine(CountN);
+    }
+    CountN++;    
+}
